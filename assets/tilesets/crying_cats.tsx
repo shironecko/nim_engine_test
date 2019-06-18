80eb4afc461a100f708f0ee568ebc12779a35d05
shironecko <shironecko.mail@gmail.com>
2019-06-18 23:26:56 +0300

    some test tiled assets

---
+++ assets/tilesets/crying_cats.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="crying_cats" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../textures/debug_atlas_small.bmp" width="128" height="128"/>
+</tileset>
